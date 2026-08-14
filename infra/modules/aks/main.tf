81dd5f6a2def304e47d3bc6059a7e1f00c27f74c
hycst <yang0371@algonquinlive.com>
2026-08-14 13:26:25 -0400

    Add AKS module for test and production

---
+++ infra/modules/aks/main.tf
@@ -0,0 +1,30 @@
+resource "azurerm_kubernetes_cluster" "main" {
+  name                = var.cluster_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  dns_prefix          = var.cluster_name
+
+  kubernetes_version = var.kubernetes_version
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  default_node_pool {
+    name           = "system"
+    vm_size        = var.vm_size
+    vnet_subnet_id = var.subnet_id
+
+    auto_scaling_enabled = var.auto_scaling_enabled
+
+    node_count = var.auto_scaling_enabled ? null : var.node_count
+    min_count  = var.auto_scaling_enabled ? var.min_count : null
+    max_count  = var.auto_scaling_enabled ? var.max_count : null
+  }
+
+  network_profile {
+    network_plugin = "azure"
+  }
+
+  tags = var.tags
+}
