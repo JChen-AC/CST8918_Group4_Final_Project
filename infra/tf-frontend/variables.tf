8a499b7c2f5c999d97eed780024d9d4b2e6de1cc
Corey Mark-Stewart <coreyms94@hotmail.com>
2026-08-13 22:31:03 -0400

    Update artifact upload action and frontend vars
    Bumps the GitHub Actions artifact upload step from `actions/upload-artifact@v3` to `@v4` in the infra plan workflow, and removes the `label_prefix` variable from `infra/tf-frontend/variables.tf` to align frontend Terraform inputs with current usage.

--- infra/tf-frontend/variables.tf
+++ infra/tf-frontend/variables.tf
@@ -1,8 +1,3 @@
-variable "label_prefix" {
-  description = "A prefix to add to all resources"
-  type        = string
-  default     = "cst8918grp4"
-}
 variable "region" {
   description = "Location of the region"
   type        = string
